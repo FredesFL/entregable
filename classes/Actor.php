851aca7c47c94d799c9ce35922ac77facd0687fc
FredesFL <fredesfl@gmail.com>
2019-07-30 18:41:33 -0300

    Entregable

---
+++ classes/Actor.php
@@ -0,0 +1,76 @@
+<?php
+// Ej. 3 Agregar la clase actor con atributos y métodos
+
+class Actor
+{
+  private $id;
+  private $first_name;
+  private $last_name;
+  private $favorite_movie_id;
+  private $rating;
+
+
+public function _construct ($first_name, $last_name, $favorite_movie_id, $rating)
+{
+  $this->first_name = $first_name;
+  $this->last_name = $last_name;
+  $this->favorite_movie_id = $favorite_movie_id;
+  $this->rating = $rating;
+
+}
+
+public function setId($id)
+{
+  $this->id = $id;
+}
+
+public function getId()
+{
+  return $this->id;
+}
+
+public function setFirst_name($first_name)
+{
+  $this->first_name = $first_name;
+}
+
+public function getFirst_name()
+{
+  return $this->first_name;
+}
+
+public function setLast_name($last_name)
+{
+  $this->last_name = $last_name;
+}
+
+public function getLast_name()
+{
+  return $this->last_name;
+}
+
+public function getFavorite_movie_id()
+{
+  return $this->favorite_movie_id;
+}
+
+public function setFavorite_movie_id($favorite_movie_id)
+{
+  $this->favorite_movie_id = $favorite_movie_id;
+}
+
+public function getRating()
+{
+  return $this->rating;
+}
+
+public function setRating($rating)
+{
+  $this->rating = $rating;
+}
+
+}
+
+
+
+ ?>
---
+++ listado-actores.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'autoload.php';
+
+$actors = DB::getAllActors();
+
+$pageTitle = 'Listado de actores';
+require_once 'partials/head.php';
+require_once 'partials/navbar.php';
+?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-striped">
+          <thead class="thead-dark">
+              <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellido</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Película Favorita</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php echo "<pre>"; var_dump($actors); exit; foreach ($actors as $actor): ?>
+              <tr>
+                <th scope="row"><?php echo $actor->getFirst_name(); ?></th>
+                <td><?php echo $actor->getLast_name(); ?></td>
+                <td><?php echo $actor->getRating(); ?></td>
+                <td><?php echo $actor->getFavorite_movie_id(); ?></td>
+              </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
